368c558094811643faaf54bd89490b95953c150f
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-14 14:39:07 +0200

    add login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2021 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wsp-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
