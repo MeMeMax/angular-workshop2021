e6e583f8c31068902be4115ab56967d17abe22d7
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 15:01:34 +0200

    add shared module and navbar component

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wsp-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
