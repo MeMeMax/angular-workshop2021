92c1ff14f76dbe67c3687bc37ef1406b6705fdf9
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 15:13:49 +0200

    add container component

---
+++ src/app/heroes/components/container/container.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2021 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wsp-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,9 +5,10 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ListComponent } from './components/list/list.component';
 import { SharedModule } from '../shared/shared.module';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
-  declarations: [DashboardComponent, ListComponent],
+  declarations: [DashboardComponent, ListComponent, ContainerComponent],
   imports: [CommonModule, HeroesRoutingModule, SharedModule]
 })
 export class HeroesModule {}
