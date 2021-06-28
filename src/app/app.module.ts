926df6e9277af18467d842b3315a4094079f6647
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 14:56:15 +0200

    add dashboard route and link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
