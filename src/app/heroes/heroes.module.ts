ab92ed8d3605bfe576acbc73b0f77cccb8a5b984
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 14:53:39 +0200

    generate heroes module

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HeroesRoutingModule]
+})
+export class HeroesModule {}
