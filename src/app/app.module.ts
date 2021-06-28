42388ef42154f417b773218810aa4c93cffaedaa
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 15:10:09 +0200

    move wsp-navbar to app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, HeroesModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, HeroesModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
