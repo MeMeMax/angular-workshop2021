b9be8469f16f11e20098e336838bc993122af580
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 15:07:43 +0200

    adapt navbar and add route to list

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [{ path: 'dashboard', component: DashboardComponent }];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
