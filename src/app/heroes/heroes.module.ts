4f78dc6c6dcd80673e195b8bdb712a621c61fdf7
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 15:04:08 +0200

    import shared module and use navbar in dashboard and list

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ListComponent } from './components/list/list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    ListComponent
-  ],
-  imports: [CommonModule, HeroesRoutingModule]
+  declarations: [DashboardComponent, ListComponent],
+  imports: [CommonModule, HeroesRoutingModule, SharedModule]
 })
 export class HeroesModule {}
