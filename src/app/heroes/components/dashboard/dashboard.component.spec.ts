dad7558a62111e13ce70865783bd8a0b62e57d03
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-06-28 14:54:07 +0200

    add dashboard component

---
+++ src/app/heroes/components/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2021 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DashboardComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [CommonModule, HeroesRoutingModule]
 })
 export class HeroesModule {}
