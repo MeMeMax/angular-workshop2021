8318d1e5b825b360d6771153a0d9a700158e4d74
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-09-13 10:43:54 +0200

    add logic to show list of heroes passed in

--- src/app/heroes/components/list/list.component.ts
+++ src/app/heroes/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'wsp-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  @Input() heroes!: Array<any>;
+
   constructor() {}
 
   ngOnInit(): void {}
