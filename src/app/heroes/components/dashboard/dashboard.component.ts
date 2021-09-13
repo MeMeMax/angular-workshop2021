b2342ccaaa7d4f0aacfc970e9c6416fa3d03e663
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2021-09-13 10:44:19 +0200

    use list component to show dashboard heroes

--- src/app/heroes/components/dashboard/dashboard.component.ts
+++ src/app/heroes/components/dashboard/dashboard.component.ts
@@ -8,11 +8,14 @@ import { HeroesService } from '../../services/heroes.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Array<any> = [];
+
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
     this.heroesService.getHeroes().subscribe((res) => {
       console.log(res);
+      this.heroes = res.slice(0, 4);
     });
   }
 }
